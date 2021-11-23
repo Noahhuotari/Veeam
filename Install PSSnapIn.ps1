09f412852fdc6a87917ada67be58e61c8b0e4597
Noahhuotari <80728873+Noahhuotari@users.noreply.github.com>
2021-11-23 14:37:38 -0600

    Delete Install PSSnapIn.ps1

--- Install PSSnapIn.ps1
+++
@@ -1,5 +0,0 @@
-C:\windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe C:\Program Files\Veeam\Backup and Replication\Console\Veeam.Backup.PowerShell.dll
-Add-PSSnapin VeeamPSSnapin
-
-Get-PSSnapin VeeamPSSnapin
-Connect-VBRServer -Server <serverFQDN>
